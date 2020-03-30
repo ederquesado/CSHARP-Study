f559edf61b6843f0372e3ca7eac4ac360be91356
ederquesado <ederquesado@gmail.com>
2020-03-30 11:24:53 -0300

    General App Configuration

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,1 +1,12 @@
-Console.WriteLine("Hello World");
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
