55545d445d980b9d915b9cfa01763d26672c0931
ederquesado <ederquesado@gmail.com>
2020-03-30 12:10:59 -0300

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,12 +1,37 @@
 using System;
 
-namespace MyApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace MyApp {
+  class Program {
+    static void Main (string[] args) {
+      //---------------Hello World
+      // string firstName ="Joca";
+      // string secondName = "Doido";
+      // Console.WriteLine($"Hello World {firstName} {secondName}");
+
+      //----------------Methods Examples
+      // string greeting = "      Hello World!       ";
+      // Console.WriteLine ($"[{greeting}]");
+
+      // string trimmedGreeting = greeting.TrimStart ();
+      // Console.WriteLine ($"[{trimmedGreeting}]");
+
+      // trimmedGreeting = greeting.TrimEnd ();
+      // Console.WriteLine ($"[{trimmedGreeting}]");
+
+      // trimmedGreeting = greeting.Trim ();
+      // Console.WriteLine ($"[{trimmedGreeting}]");
+
+      // string sayHello="Hello World";
+      // Console.WriteLine(sayHello);
+      // sayHello=sayHello.Replace("Hello", "Greetings");
+      // Console.WriteLine(sayHello);
+
+      string songLyrics = "You say goodbye, and I say hello";
+      Console.WriteLine (songLyrics.Contains ("goodbye"));
+      Console.WriteLine (songLyrics.Contains ("greetings"));
+
+      Console.WriteLine (songLyrics.EndsWith ("hello"));
+      Console.WriteLine (songLyrics.StartsWith ("greetings"));
     }
+  }
 }
--- obj/Debug/netcoreapp3.1/MyApp.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/MyApp.AssemblyInfo.cs
@@ -10,13 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly : System.Reflection.AssemblyCompanyAttribute ("MyApp")]
+[assembly : System.Reflection.AssemblyConfigurationAttribute ("Debug")]
+[assembly : System.Reflection.AssemblyFileVersionAttribute ("1.0.0.0")]
+[assembly : System.Reflection.AssemblyInformationalVersionAttribute ("1.0.0")]
+[assembly : System.Reflection.AssemblyProductAttribute ("MyApp")]
+[assembly : System.Reflection.AssemblyTitleAttribute ("MyApp")]
+[assembly : System.Reflection.AssemblyVersionAttribute ("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
-
