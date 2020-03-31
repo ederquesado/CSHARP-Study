2893113b1492ab95bd841e431f079d52b671b551
ederquesado <ederquesado@gmail.com>
2020-03-31 10:16:56 -0300

    Add examples

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -32,6 +32,20 @@ namespace MyApp {
 
       Console.WriteLine (songLyrics.EndsWith ("hello"));
       Console.WriteLine (songLyrics.StartsWith ("greetings"));
+
+      if((songLyrics.Contains ("goodbye"))){
+          Console.WriteLine ("Tem tetra ai");
+      }
+
+      int sum = 0;
+      for (int number = 1; number < 21; number++)
+      {
+        if (number % 3 == 0)
+        {
+          sum = sum + number;
+        }
+      }
+      Console.WriteLine($"The sum is {sum}");
     }
   }
 }
